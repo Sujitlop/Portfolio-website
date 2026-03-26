9a897d056887ccd97e141e1e10b211ac01787107
Your Full Name <lopchansujeet@gmail.com>
2026-03-26 01:32:07 -0500

    Add new project card in Projects section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -27,6 +27,18 @@ function Projects() {
             />
           </Col>
         </Row>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chatify}
+              isBlog={false}
+              title="AWS AI Chatbot - Summer Project"
+              description="Developed an AI chatbot using Amazon Bedrock and Lambda to address university queries related to tuition, MFA, Workday, and other services. This solution replaced an outdated chatbot that was never helpful enough, eliminating the need to spend a lot of time on phone calls. The chatbot significantly reduced support call times from 30-40 minutes, improving overall helpdesk efficiency. Built with Python, Amazon Bedrock, Lambda, and CloudWatch for monitoring."
+              ghLink="https://github.com/sujitlopchan"
+              demoLink="https://aws.com/sujit/repo"
+            />
+          </Col>
+        </Row>
       </Container>
     </Container>
   );
